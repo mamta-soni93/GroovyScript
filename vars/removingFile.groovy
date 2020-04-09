381c73306315634e82f6ba729a08d17f3b87ab53
mamta-soni22 <mamtasoni.atcs@gmail.com>
2020-04-09 13:45:07 +0530

    Function in grovy

---
+++ vars/removingFile.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+//Removing a file
+def call(String path, String fileName, String fileExtension) {
+  dir (path) {
+    echo "Removing file : ${fileName}${fileExtension}"
+    bat 'dir' //ls -la
+	bat "del -f ${fileName}${jarExtension}"  // rm -f
+	echo "File is successfully removed..!"
+  }
+}
