63926fb986f3ccde55374ba69ceb4c9c5a9b6091
mamta-soni22 <mamtasoni.atcs@gmail.com>
2020-04-10 16:15:58 +0530

    Changes in script

--- vars/downloadArtifact.groovy
+++ vars/downloadArtifact.groovy
@@ -1,9 +1,11 @@
 #!/usr/bin/env groovy
 // File Downloading method
-def call() {
-	bat '''
-        curl -sSf -H "X-JFrog-Art-Api:AKCp5ekmsgbnFTK8mAWyiHq3W9q6KuDKGwBAjvNzvT5A2Vst1j4xHSZq3oPwC8V5jmLEqz3dQ" \
-	   -o F:/InfoObjects/Jenkins/Download/test.jar https://hexxa.jfrog.io/artifactory/Jenkins-integration/jb-hello-world-maven-0.1.0.jar
-    '''
+def call(String credentialsId) {
+	withCredentials([usernamePassword(credentialsId: '${credentialsId}', usernameVariable: 'user', passwordVariable: 'password')]) {
+        bat '''
+         curl -sSf -H "X-JFrog-Art-Api:AKCp5ekwC4jCuLH48Gf3z8V8CwSVEtNtfZ9ZcGvwygHBpoqDzjaaZYG8dLHW3yFQFSi7vSZ4S" \
+	   -o F:/InfoObjects/Jenkins/Download/test.jar https://akku.jfrog.io/artifactory/Jenkins-integration/org/springframework/jb-hello-world-maven/0.1.0/jb-hello-world-maven-0.1.0.jar
+        '''
+    }
 	echo "Jar file downloading is complete"
 }
