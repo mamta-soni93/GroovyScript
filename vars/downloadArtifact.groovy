f60256ab9fff19d6b96bc4730723d247891161c8
mamta-soni22 <mamtasoni.atcs@gmail.com>
2020-04-10 16:18:58 +0530

    Changes in script

--- vars/downloadArtifact.groovy
+++ vars/downloadArtifact.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 // File Downloading method
-def call(String credentialsId) {
-	withCredentials([usernamePassword(credentialsId: '${credentialsId}', usernameVariable: 'user', passwordVariable: 'password')]) {
+def call(String Artifactory_Credential_ID) {
+	withCredentials([usernamePassword(credentialsId: "${Artifactory_Credential_ID}", usernameVariable: 'user', passwordVariable: 'password')]) {
         bat '''
          curl -sSf -H "X-JFrog-Art-Api:AKCp5ekwC4jCuLH48Gf3z8V8CwSVEtNtfZ9ZcGvwygHBpoqDzjaaZYG8dLHW3yFQFSi7vSZ4S" \
 	   -o F:/InfoObjects/Jenkins/Download/test.jar https://akku.jfrog.io/artifactory/Jenkins-integration/org/springframework/jb-hello-world-maven/0.1.0/jb-hello-world-maven-0.1.0.jar
